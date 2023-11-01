0aa2f35c884270ed76062a8065fa37841cc1d6fe
Amanda Moreira <amandallmoreira@gmail.com>
2023-10-31 21:52:27 -0300

    v3
    Atualização de funções.

--- config/processamento.php
+++ config/processamento.php
@@ -1,12 +1,28 @@
 <?php 
 include_once("conexao.php");
 include_once("url.php");
+$id;
 
-$query="SELECT*FROM contatos"; //string que manipula a tabela SQL;
+if(!empty($_GET)){  //SÓ ENTRA NO IF SE FOR VERDADEIRO, ! É NEGAÇÃO, SE NEGA UM f ELE VIRA v.
+    $id=$_GET["id"];
 
-$stmt=$conn->prepare($query);
-$stmt->execute();  //executa uma consulta no Banco de Dados;
+}//fim IF
+if(!empty($id)){ //não retorna nada
+    $query="SELECT*FROM contatos WHERE id=:id";
+    $stmt=$conn->prepare($query);
+    $stmt->bindParam(":id",$id);  
+    $stmt->execute();
+    $onlyContato=$stmt->fetch();  //Retorna apenas a linha referente ao ID.
+}
 
-$AllContatos=[];
-$AllContatos=$stmt->fetchAll();  //Retorna todas as linhas que tem na tabela Contatos.
+else{
+    $query="SELECT*FROM contatos"; //string que manipula a tabela SQL;
+    $stmt=$conn->prepare($query);
+    $stmt->execute();  //executa uma consulta no Banco de Dados;
+    $AllContatos=[];
+    $AllContatos=$stmt->fetchAll();  //Retorna todas as linhas que tem na tabela Contatos.
+
+}//fim ELSE
+
+
 ?>
